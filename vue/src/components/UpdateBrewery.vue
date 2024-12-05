1c92de17882c907edce35f1bd0cd9b8b4327a4db
Martina Kyanya <tinakyanya@gmail.com>
2024-12-05 13:26:42 -0500

    cap

--- vue/src/components/UpdateBrewery.vue
+++ vue/src/components/UpdateBrewery.vue
@@ -1,55 +1,82 @@
 <template>
 
-  <div>test</div>
 
-  <form >
-
+  <form>
     <h2>Update Brewery</h2>
-    
+
     <div>
       <label for="name"> Brewery Name: </label>
-      <input id="name" type="text">
+      <input id="name" type="text" v-model="brewery.breweryName" />
     </div>
     <div>
       <label for="brewerId">Brewer ID: </label>
-      <input id="brewerId" type="text">
+      <input id="brewerId" type="text"  v-model="brewery.userId"/>
     </div>
     <div>
       <label for="description">Description: </label>
-      <input id="description" type="text">
+      <input id="description" type="text"  v-model="brewery.description" />
     </div>
     <div>
       <label for="address"> Address: </label>
-      <input id="address" type="text">
+      <input id="address" type="text"  v-model="brewery.address" />
     </div>
     <div>
       <label for="city"> City: </label>
-      <input id="city" type="text">
+      <input id="city" type="text"  v-model="brewery.city" />
     </div>
     <div>
-      <label for="state"> State:  </label>
-      <input id="state" type="text">
+      <label for="state"> State: </label>
+      <input id="state" type="text"  v-model="brewery.state"/>
     </div>
     <div>
-      <label for="zipcode"> Zipcode:  </label>
-      <input id="zipcode" type="text">
+      <label for="zipcode"> Zipcode: </label>
+      <input id="zipcode" type="text"  v-model="brewery.zipcode"/>
     </div>
     <button class="btn btn-submit">Submit</button>
-        <button class="btn btn-cancel" v-on:click="cancelForm" type="button">Cancel</button>
-
+    <button class="btn btn-cancel" v-on:click="cancelForm" type="button">
+      Cancel
+    </button>
   </form>
-
-
-
 </template>
 
 
 
 
 <script>
+import BreweryService from "../services/BreweryService";
+
 export default {
+  data() {
+    return {
+      brewery: {
+        breweryName: "",
+        userId: "",
+        description: "",
+        address: "",
+        city: "",
+        state: "",
+        zipcode: "",
+      },
+    };
+  },
 
+  created() {
+    this.getBreweryData();
+  },
 
+  methods: {
+    getBreweryData() {
+      const breweryId = this.$route.params.id;
+
+      BreweryService.getBreweryById(breweryId)
+        .then((response) => {
+          this.brewery = response.data;
+        })
+        .catch((error) => {
+          console.error("Error fetching brewery details:", error);
+        });
+    },
+  },
 };
 </script>
 
@@ -57,5 +84,4 @@ export default {
 
 
 <style scoped>
-
 </style>
