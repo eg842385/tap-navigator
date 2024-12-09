b0347d791e29d9d9680ec0c954205b1a6ef366ac
Erika Williamson <egstroh23@gmail.com>
2024-12-08 20:19:23 -0500

    fixed spacing

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -4,8 +4,8 @@
       <router-link v-bind:to="{ name: 'home' }">Home</router-link>&nbsp;|&nbsp;
       <router-link v-bind:to="{ name: 'login' }">Login</router-link>&nbsp;|&nbsp;
 
-      <router-link v-bind:to="{ name: 'listBreweries' }"> Breweries</router-link>
-      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''">Logout</router-link> &nbsp;|&nbsp;
+      <router-link v-bind:to="{ name: 'listBreweries' }"> Breweries</router-link>&nbsp;|&nbsp;
+      <router-link v-bind:to="{ name: 'logout' }" v-if="$store.state.token != ''"> Logout</router-link> &nbsp;|&nbsp;
     </div>
     <router-view />
   </div>
